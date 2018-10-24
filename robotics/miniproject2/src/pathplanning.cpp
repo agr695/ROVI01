6090a8ce7ed0f3a897e6067df4533f3955545523
agr695 <agr695@gmail.com>
2018-10-24 17:17:56 +0200

    Add Lua wrtter function

--- robotics/miniproject2/src/pathplanning.cpp
+++ robotics/miniproject2/src/pathplanning.cpp
@@ -1,9 +1,10 @@
-
 #include <iostream>
 #include <rw/rw.hpp>
 #include <rwlibs/pathplanners/rrt/RRTPlanner.hpp>
 #include <rwlibs/pathplanners/rrt/RRTQToQPlanner.hpp>
 #include <rwlibs/proximitystrategies/ProximityStrategyFactory.hpp>
+#include <fstream>
+
 
 using namespace std;
 using namespace rw::common;
@@ -18,7 +19,7 @@ using namespace rwlibs::pathplanners;
 using namespace rwlibs::proximitystrategies;
 
 #define MAXTIME 10.
-
+void export2LUA(QPath path);
 bool checkCollisions(Device::Ptr device, const State &state, const CollisionDetector &detector, const Q &q) {
 	State testState;
 	CollisionDetector::QueryResult data;
@@ -40,14 +41,13 @@ bool checkCollisions(Device::Ptr device, const State &state, const CollisionDete
 }
 
 int main(int argc, char** argv) {
-	const string wcFile = "../Kr16WorkCell/Scene.wc.xml";
+	rw::math::Math::seed();
+	const string wcFile = "../Kr16WallWorkCell/Scene.wc.xml";
 	const string deviceName = "KukaKr16";
 	cout << "Trying to use workcell " << wcFile << " and device " << deviceName << endl;
 
-	
 	WorkCell::Ptr wc = WorkCellLoader::Factory::load(wcFile);
 	Device::Ptr device = wc->findDevice(deviceName);
-
 	if (device == NULL) {
 		cerr << "Device: " << deviceName << " not found!" << endl;
 		return 0;
@@ -66,12 +66,12 @@ int main(int argc, char** argv) {
 	/** More complex way: allows more detailed definition of parameters and methods */
 	QSampler::Ptr sampler = QSampler::makeConstrained(QSampler::makeUniform(device),constraint.getQConstraintPtr());
 	QMetric::Ptr metric = MetricFactory::makeEuclidean<Q>();
-	double extend = 0.1;
+	double extend = 0.5;
 	QToQPlanner::Ptr planner = RRTPlanner::makeQToQPlanner(constraint, sampler, metric, extend, RRTPlanner::RRTConnect);
 
-	Q from(6,-0.2,-0.6,1.5,0.0,0.6,1.2);
+	Q from(6,-3.142,-0.827,-3.002,-3.143,0.099,-1.573);
 	//Q to(6,1.7,0.6,-0.8,0.3,0.7,-0.5); // Very difficult for planner
-	Q to(6,1.4,-1.3,1.5,0.3,1.3,1.6);
+	Q to(6,1.571,0.006,0.030,0.153,0.762,4.490);
 
 	if (!checkCollisions(device, state, detector, from))
 		return 0;
@@ -94,5 +94,54 @@ int main(int argc, char** argv) {
 	}
 
 	cout << "Program done." << endl;
+	export2LUA(path);
 	return 0;
+}
+
+void export2LUA(QPath path){
+	ofstream LuaFile;
+  LuaFile.open ("../LUA/LuaScript.lua",ios::out|ios::trunc);
+  LuaFile << "wc = rws.getRobWorkStudio():getWorkCell()\n";
+  LuaFile << "state = wc:getDefaultState()\n";
+  LuaFile << "device = wc:findDevice(\"KukaKr16\")\n";
+  LuaFile << "gripper = wc:findFrame(\"Tool\");\n";
+  LuaFile << "bottle = wc:findFrame(\"Bottle\");\n";
+  LuaFile << "table = wc:findFrame(\"Table\");\n";
+  LuaFile << "\n";
+
+  LuaFile << "function setQ(q)\n";
+  LuaFile << "qq = rw.Q(#q,q[1],q[2],q[3],q[4],q[5],q[6])\n";
+  LuaFile << "device:setQ(qq,state)\n";
+  LuaFile << "rws.getRobWorkStudio():setState(state)\n";
+  LuaFile << "rw.sleep(0.1)\n";
+  LuaFile << "end\n";
+  LuaFile << "\n";
+
+  LuaFile << "function attach(obj, tool)\n";
+  LuaFile << "rw.gripFrame(obj, tool, state)\n";
+  LuaFile << "rws.getRobWorkStudio():setState(state)\n";
+  LuaFile << "rw.sleep(0.1)\n";
+  LuaFile << "end\n";
+
+  LuaFile << "\n";
+  LuaFile << "\n";
+  LuaFile << "setQ({-3.142, -0.827, -3.002, -3.143, 0.099, -1.573})\n";
+  LuaFile << "attach(bottle,gripper)\n\n";
+	string string_Q;
+	ostringstream ss;
+	for (QPath::iterator it = path.begin()+1; it < path.end()-1; it++) {
+		ss << *it;
+		string_Q=ss.str();
+		string_Q.erase(0, 4);
+		LuaFile << "setQ("<< string_Q <<")\n\n";
+		ss.str("");
+		ss.clear();
+		// LuaFile << *it;
+	}
+
+	LuaFile << "\n";
+  LuaFile << "setQ({1.571, 0.006, 0.03, 0.153, 0.762, 4.49})\n";
+	LuaFile << "attach(bottle,table)\n";
+
+  LuaFile.close();
 }
